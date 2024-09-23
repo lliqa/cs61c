48cd5aeb5155713bcb360be7a9887abdcad19993
lliqa <llcct@outlook.com>
2024-09-23 11:29:13 +0800

    Finished lab01

--- lab01/ll_cycle.c
+++ lab01/ll_cycle.c
@@ -3,5 +3,16 @@
 
 int ll_has_cycle(node *head) {
     /* your code here */
+    node *tort, *hare;
+    tort = hare = head;
+    if (head == NULL) return 0;
+    while (1) {
+        hare = hare->next;
+        if (hare == NULL) return 0;
+        hare = hare->next;
+        if (hare == NULL) return 0;
+        tort = tort->next;
+        if (hare == tort) return 1;
+    }
     return 0;
 }
