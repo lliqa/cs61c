593c108fb04bdf77ca921d5a41c5bb05e42e3dc0
lliqa <llcct@outlook.com>
2024-10-09 17:15:17 +0800

    Finished lab03

--- lab03/ex2.s
+++ lab03/ex2.s
@@ -31,17 +31,17 @@ fun:
 main:
     # BEGIN PROLOGUE
     addi sp, sp, -20
-    sw s0, 0(sp)
+    sw s0, 0(sp) 
     sw s1, 4(sp)
     sw s2, 8(sp)
     sw s3, 12(sp)
     sw ra, 16(sp)
     # END PROLOGUE
-    addi t0, x0, 0
-    addi s0, x0, 0
-    la s1, source
-    la s2, dest
-loop:
+    addi t0, x0, 0 # t0 -> k
+    addi s0, x0, 0 # s0 -> sum
+    la s1, source # s1 -> source
+    la s2, dest # s2 -> dest
+loop: # loop
     slli s3, t0, 2
     add t1, s1, s3
     lw t2, 0(t1)
